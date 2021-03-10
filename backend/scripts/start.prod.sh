76794c9bdd8522d46b09734b09ab2330c41bd909
Dmytro <dimashevchenko10021999@gmail.com>
2021-03-10 06:56:05 +0200

    Use docker in project

---
+++ backend/scripts/start.prod.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+if [[ "$DATABASE" = "postgres" ]]
+then
+    echo "Waiting for postgres..."
+
+    while ! nc -z $DJANGO_SETTINGS_DATABASES_DEFAULT_HOST $DJANGO_SETTINGS_DATABASES_DEFAULT_PORT; do
+      sleep 0.1
+    done
+
+    echo "PostgreSQL started"
+fi
+
+rm ./*/migrations/0*.py
+python manage.py makemigrations
+python manage.py migrate --no-input
+python manage.py collectstatic --no-input --clear
+python manage.py create_custom_superuser
+python manage.py create_books
+python manage.py runserver 0.0.0.0:8000
