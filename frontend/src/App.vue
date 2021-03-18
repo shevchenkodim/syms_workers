4f999c4d8886d8f71ed62836fe16c388caa413e3
Dmytro <dimashevchenko10021999@gmail.com>
2021-03-19 00:03:47 +0200

    Main page by vuetifyjs

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,12 +1,180 @@
 <template>
   <v-app>
-    <router-view></router-view>
+    <div>
+      <v-navigation-drawer
+        app
+        dark
+        permanent
+      >
+        <v-list-item class="px-2 py-1">
+          <v-list-item-avatar>
+            <v-img src="https://randomuser.me/api/portraits/men/85.jpg"></v-img>
+          </v-list-item-avatar>
+
+          <v-list-item-title>John Leider</v-list-item-title>
+        </v-list-item>
+
+        <v-divider></v-divider>
+
+        <v-list dense>
+          <v-list-item
+            v-for="item in items"
+            :key="item.title"
+            link
+          >
+            <v-list-item-icon>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-item-icon>
+
+            <v-list-item-content>
+              <v-list-item-title>{{ item.title }}</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+      </v-navigation-drawer>
+    </div>
+    <v-app-bar
+      app
+      elevate-on-scroll
+      dark
+      height="64"
+    >
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Page title</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+
+      <v-menu
+        v-model="menu"
+        :close-on-content-click="false"
+        :nudge-width="200"
+        offset-x
+        bottom
+      >
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn
+            icon
+            dark
+            v-bind="attrs"
+            v-on="on"
+          >
+            <v-icon>mdi-account</v-icon>
+          </v-btn>
+        </template>
+
+        <v-card>
+          <v-list>
+            <v-list-item>
+              <v-list-item-avatar>
+                <img
+                  src="https://cdn.vuetifyjs.com/images/john.jpg"
+                  alt="John"
+                >
+              </v-list-item-avatar>
+
+              <v-list-item-content>
+                <v-list-item-title>John Leider</v-list-item-title>
+                <v-list-item-subtitle>Founder of Vuetify</v-list-item-subtitle>
+              </v-list-item-content>
+
+              <v-list-item-action>
+                <v-btn
+                  :class="fav ? 'red--text' : ''"
+                  icon
+                  @click="fav = !fav"
+                >
+                  <v-icon>mdi-heart</v-icon>
+                </v-btn>
+              </v-list-item-action>
+            </v-list-item>
+          </v-list>
+
+          <v-divider></v-divider>
+
+          <v-list>
+            <v-list-item>
+              <v-list-item-action>
+                <v-switch
+                  v-model="message"
+                  color="purple"
+                ></v-switch>
+              </v-list-item-action>
+              <v-list-item-title>Enable messages</v-list-item-title>
+            </v-list-item>
+
+            <v-list-item>
+              <v-list-item-action>
+                <v-switch
+                  v-model="hints"
+                  color="purple"
+                ></v-switch>
+              </v-list-item-action>
+              <v-list-item-title>Enable hints</v-list-item-title>
+            </v-list-item>
+          </v-list>
+
+          <v-card-actions>
+            <v-spacer></v-spacer>
+
+            <v-btn
+              text
+              @click="menu = false"
+            >
+              Cancel
+            </v-btn>
+            <v-btn
+              color="primary"
+              text
+              @click="menu = false"
+            >
+              Save
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-menu>
+
+    </v-app-bar>
+
+    <v-main>
+      <v-container fluid></v-container>
+    </v-main>
+
+    <v-footer app dark class="d-flex justify-center">
+        Developed by
+        <a href="https://www.instagram.com/dima_shevchenko99/">
+          Dmytro Shevchenko
+        </a>
+    </v-footer>
   </v-app>
 </template>
 
 <script>
 export default {
   name: 'App',
-  data: () => ({})
+  data: () => ({
+    drawer: true,
+    items: [
+      { title: 'Home', icon: 'mdi-home-city' },
+      { title: 'My Account', icon: 'mdi-account' },
+      { title: 'Users', icon: 'mdi-account-group-outline' }
+    ],
+    mini: true,
+    fav: true,
+    menu: false,
+    message: false,
+    hints: true
+  })
 }
 </script>
+
+<style>
+</style>
